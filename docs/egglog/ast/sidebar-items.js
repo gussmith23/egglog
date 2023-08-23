511cf8de976b0eb3ad5b78a3416f5cb48787f242
mwillsey <mwillsey@users.noreply.github.com>
2023-08-23 16:59:02 +0000

    deploy: 9e530381961a59524f2bbacd89973575b4e036d8

---
+++ docs/egglog/ast/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Action","Command","Expr","Fact","Literal","NCommand","NormAction","NormExpr","NormFact","NormSchedule","Schedule"],"mod":["desugar","parse"],"struct":["FunctionDecl","Id","IdentSort","Metadata","NormCommand","NormRule","NormRunConfig","Rewrite","Rule","RunConfig","Schema","Symbol","Variant"],"type":["CommandId"]};
